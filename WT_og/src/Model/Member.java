93c735809a1e6c55a5c6f5b34fac5a9ef8dccf17
Yijin Wang <ymw5567@psu.edu>
2026-04-06 00:17:01 -0400

    design classes again, and write basic code for classes.

---
+++ WT_og/src/Model/Member.java
@@ -0,0 +1,39 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class Member {
+    private final int id;
+    private String userName; //The name that shows to other users
+    private String password;
+    private final LocalDateTime createdAt;
+
+    public Member(int id, String userName, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+    //getter and setter
+
+    public int getId() {return id;}
+
+    public String getUserName() {return userName;}
+    public void setUserName(String userName) {this.userName = userName;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
+
+    public LocalDateTime getCreatedAt() {return createdAt;}
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
--- WT_og/src/Model/PeopleCalendar.java
+++
@@ -1,4 +0,0 @@
-package Model;
-
-public class PeopleCalendar extends Calendar{
-}
