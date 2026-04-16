3073cffaebf6e90f249ddeffa2b92844a4480f9d
Yijin Wang <ymw5567@psu.edu>
2026-04-16 10:50:43 -0400

    Created an ArrayList of Song in class Event

--- WT_og/src/Model/Event.java
+++ WT_og/src/Model/Event.java
@@ -1,16 +1,19 @@
 package Model;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class Event {
     private LocalDate date;
     private LocalTime time;
     private String title;  //topic of the sermon
+    private ArrayList<Song> songs;
 
-    public Event(LocalDate date, LocalTime time, String title) {
+    public Event(LocalDate date, LocalTime time, String title, ArrayList<Song> songs) {
         this.date = date;
         this.time = time;
         this.title = title;
+        this.songs = songs;
     }
 
     //getter and setter
@@ -24,6 +27,17 @@ public class Event {
     public String getTitle() {return title;}
     public void setTitle(String title) {this.title = title;}
 
+    public ArrayList<Song> getSongs() {return songs;} //notice: returned an ArrayList
+
+    public void addSong(Song song) {
+        if (songs.contains(song)) {return;} //if the song already exist, don't do anything
+        songs.add(song);
+    }
+
+    public void removeSong(Song song) {
+        if (!songs.contains(song)) {return;} //if the song doesn't exist, don't do anything
+        songs.remove(song);
+    }
 
     @Override
     public String toString() {
